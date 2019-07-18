ef4ac97804f51bc71b10d0f6d37cef37633b2bcb
qchuchu <quentin.churet@outlook.fr>
2019-07-18 19:03:50 +0200

    Last commit

--- spec/deputes_spec.rb
+++ spec/deputes_spec.rb
@@ -4,8 +4,8 @@ require_relative '../lib/deputes'
 
 describe 'get_deputy_info' do
   it 'check if the deputy is well gotten' do
-    expect(get_deputy_info('/deputes/fiche/OMC_PA720614')).to include(hash_including(first_name="Marine", last_name=>"Le Pen"))
-    expect(get_deputy_info('/deputes/fiche/OMC_PA721984')).to include(hash_including(first_name=>"Philippe",last_name=>"Latombe"))
+    expect(get_deputy_info('/deputes/fiche/OMC_PA720614')).to include(hash_including('first_name'="Marine", 'last_name'=>"Le Pen"))
+    expect(get_deputy_info('/deputes/fiche/OMC_PA721984')).to include(hash_including('first_name'=>"Philippe",'last_name'=>"Latombe"))
   end
 end
 
@@ -19,6 +19,6 @@ end
 describe 'perform' do
   it 'check if profile is well gotten' do
     expect(perform) \
-      .to include(hash_including(first_name=>"Marine", last_name=>"Le Pen"), hash_including(first_name=>"Philippe", last_name=>"Latombe"))
+      .to include(hash_including('first_name'=>"Marine", 'last_name'=>"Le Pen"), hash_including('first_name'=>"Philippe", 'last_name'=>"Latombe"))
   end
 end
